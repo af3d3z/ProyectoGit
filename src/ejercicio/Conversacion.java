663f105fd7defcf3024f8d1a006c73fbb43947fb
nestorss <109480368+nestoorss@users.noreply.github.com>
2024-01-18 22:26:56 +0100

    Añadido un medidor de alegria para la maquina

--- src/ejercicio/Conversacion.java
+++ src/ejercicio/Conversacion.java
@@ -9,4 +9,14 @@ public class Conversacion {
 	public void saludar() {
 		System.out.println("Hola Mundo!");
 	}
+	
+	/***
+	 * Comprueba el estado de felicidad por si la maquina opta por suspenderte el curso entero
+	 * @return el indice de felicidad de la maquina
+	 */
+	public int estadoAnimo() {
+		int felicidad;
+		felicidad = (int) (Math.random()*2);
+		return felicidad;
+	}
 }
--- src/ejercicio/Main.java
+++ src/ejercicio/Main.java
@@ -4,5 +4,10 @@ public class Main {
 	public static void main(String[] args) {
 		Conversacion c = new Conversacion();
 		c.saludar();
+		if (c.estadoAnimo()==1) {
+			System.out.println("¡Estoy de buen humor!");
+		} else {
+			System.out.println("Hoy te van a dar por todos lados. Te suspendere todas las asignaturas porque puedo y porque quiero.");
+		}	
 	}
 }
